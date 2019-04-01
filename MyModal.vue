a056f4374967ed5d2aa2df523a2418dba08f029b
Masatoshi Ishikawa <goetan.musicbox@nifty.com>
2019-04-01 14:00:53 +0900

    at 2019.03.01 12:35

--- MyModal.vue
+++ MyModal.vue
@@ -1,65 +1,37 @@
 <template>
-    <transition name="modal" appear>
-        <div class="modal modal-overlay" @click.self="$emit('close')">
-            <div class="modal-window">
-                <div class="modal-content">
-                    <slot/>
+    <div>
+        <h2>お知らせ</h2>
+        <transition name="modal">
+            <div class="modal" v-if='showModal'>
+                <div class="modal_inner">
+                    <p>合計をクリップボードへコピーしました。</p>
+                    <a class="close" href="#" v-on:click='click01'>×</a>
                 </div>
-                <footer class="modal-footer">
-                    <slot name="footer">
-                        <button @click="$emit('close')">Close</button>
-                    </slot>
-                </footer>
             </div>
-        </div>
-    </transition>
+        </transition>
+    </div>
 </template>
 
-<style lang="stylus" scoped>
-    .modal {
-        &.modal-overlay {
-            display: flex;
-            align-items: center;
-            justify-content: center;
-            position: fixed;
-            z-index: 30;
-            top: 0;
-            left: 0;
-            width: 100%;
-            height: 100%;
-            background: rgba(0, 0, 0, 0.5);
-        }
-        &-window {
-            background: #fff;
-            border-radius: 4px;
-            overflow: hidden;
-        }
-        &-content {
-            padding: 10px 20px;
-        }
-        &-footer {
-            background: #ccc;
-            padding: 10px;
-            text-align: right;
+<script>
+    export default {
+        data(){
+            return {
+                showModal: false
+            }
+        },methods: {
+            click01: function (ev) {
+                this.showModal = false
+                ev.preventDefault()
+            }
         }
     }
-    // オーバーレイのトランジション
+</script>
+
+<style>
     .modal-enter-active, .modal-leave-active {
-        transition: opacity 0.4s;
-        // オーバーレイに包含されているモーダルウィンドウのトランジション
-        .modal-window {
-            transition: opacity 0.4s, transform 0.4s;
-        }
-    }
-    // ディレイを付けるとモーダルウィンドウが消えた後にオーバーレイが消える
-    .modal-leave-active {
-        transition: opacity 0.6s ease 0.4s;
+        transition:opacity 0.5s;
     }
     .modal-enter, .modal-leave-to {
         opacity: 0;
-        .modal-window {
-            opacity: 0;
-            transform: translateY(-20px);
-        }
     }
 </style>
