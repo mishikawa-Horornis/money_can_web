bd63374b28e8c94e8bcfbb31e6ef21a815ec9298
goemon-Horornis <goetan.musicbox@nifty.com>
2019-08-25 09:06:14 +0900

    at 2019.08.25 9:06

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 let PRECACHE = 'moneycounter-horornis-v3_0_3';
 let RUNTIME = 'runtime';
-let version = '3.1.488';
+let version = '3.1.489';
 
 // A list of local resources we always want to be cached.
 let PRECACHE_URLS = [
