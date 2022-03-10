6081dbdc7b875a60054fa483dfdcc673269bab73
Masatoshi Ishikawa <massagoegoe@gmail.com>
2022-03-10 13:04:01 +0900

    修正

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 let PRECACHE = 'moneycounter-horornis-v3_0_3';
 let RUNTIME = 'runtime';
-let version = '1.10.22';
+let version = '1.10.27';
 
 // A list of local resources we always want to be cached.
 let PRECACHE_URLS = [
