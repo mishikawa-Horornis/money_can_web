3a274b89f1b9be187255e5eccbfe6022250ae5b5
mishikawa-Horornis <Massagoegoe-8192>
2022-03-13 19:23:36 +0900

    Changes

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 let PRECACHE = 'moneycounter-horornis-v3_0_3';
 let RUNTIME = 'runtime';
-let version = '1.10.31';
+let version = '1.10.32';
 
 // A list of local resources we always want to be cached.
 let PRECACHE_URLS = [
